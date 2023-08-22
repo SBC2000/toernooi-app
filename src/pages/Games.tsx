90404822d229851e0818b0204d48c4fbeec0955d
Vincent van der Weele <vincentvanderweele@hotmail.com>
2023-08-22 09:21:48 +0300

    Show games on games page

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -1,3 +1,114 @@
+import { Tab, Table, TabList, TabPanel, Tabs } from '@mui/joy'
+import { Fragment } from 'react'
+import { Link } from 'react-router-dom'
+import Leaderboard from '@mui/icons-material/Leaderboard'
+
+import './Games.css'
+import { useDatabase } from '../hooks/useDatabase'
+import { useGamesByField } from '../hooks/useGamesByField'
+
 export default function Games() {
-  return <div>Games</div>
+  const database = useDatabase(0)
+  const gamesByField = useGamesByField(database)
+
+  return (
+    <div className="App">
+      <Tabs>
+        {gamesByField.map(({ gameDays }, i) => (
+          <TabPanel value={i} key={i}>
+            {gameDays.map(({ date, games }) => (
+              <Fragment key={date}>
+                <div
+                  style={{
+                    position: 'sticky',
+                    top: 0,
+                    backgroundColor: 'white',
+                    padding: 'var(--Tabs-spacing)',
+                  }}
+                >
+                  {date}
+                </div>
+                <Table>
+                  <thead>
+                    <tr>
+                      <th style={{ height: 0, width: '10%' }}></th>
+                      <th style={{ height: 0 }}></th>
+                      <th style={{ height: 0, width: '10%' }}></th>
+                      <th style={{ height: 0 }}></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {games.map((game) => (
+                      <tr key={game.id}>
+                        <td>
+                          <div>{formatTime(game.date)}</div>
+                          <div>{game.poolAbbreviation}</div>
+                        </td>
+                        <td>
+                          <div className="game-table-cell">
+                            {game.homeTeamName}
+                          </div>
+                          <div className="game-table-cell">
+                            {game.referee1?.name ?? '\u00A0'}
+                          </div>
+                        </td>
+                        <td>
+                          {game.result?.homeScore ?? ''} -{' '}
+                          {game.result?.awayScore ?? ''}
+                        </td>
+                        <td>
+                          <div className="game-table-cell">
+                            {game.awayTeamName}
+                          </div>
+                          <div className="game-table-cell">
+                            {game.referee2?.name ?? '\u00A0'}
+                          </div>
+                        </td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </Table>
+              </Fragment>
+            ))}
+          </TabPanel>
+        ))}
+        <TabPanel value={gamesByField.length}></TabPanel>
+        <TabList sticky="bottom" underlinePlacement="top">
+          {gamesByField.map(({ fieldName }) => (
+            <Tab
+              variant="plain"
+              color="warning"
+              indicatorPlacement="top"
+              key={fieldName}
+            >
+              {fieldName}
+            </Tab>
+          ))}
+        </TabList>
+      </Tabs>
+      <Link
+        to="/standings"
+        style={{
+          position: 'fixed',
+          right: 0,
+          bottom: 0,
+          width: '40px',
+          height: '40px',
+          color: 'red',
+          zIndex: 100,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Leaderboard></Leaderboard>
+      </Link>
+    </div>
+  )
+}
+
+function formatTime(date: Date) {
+  return [date.getHours(), date.getMinutes()]
+    .map((x) => `0${x}`.slice(-2))
+    .join(':')
 }
