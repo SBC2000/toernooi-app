4045a5aad6ed4b549c9982ec30897ef939030851
Vincent van der Weele <vincentvanderweele@hotmail.com>
2023-08-26 22:17:04 +0300

    Style homepage

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,21 @@
 import { Link } from 'react-router-dom'
 
-export default function Header(props: { title: string }) {
-  const { title } = props
+export default function Header(props: {
+  title: string
+  hideBackButton?: boolean
+}) {
+  const { title, hideBackButton } = props
 
   return (
     <>
-      <Link
-        className="absolute  text-sbcOrange-500 text-3xl font-medium px-2"
-        to="/"
-      >
-        {'\u2039'}
-      </Link>
+      {hideBackButton || (
+        <Link
+          className="absolute  text-sbcOrange-500 text-3xl font-medium px-2"
+          to="/"
+        >
+          {'\u2039'}
+        </Link>
+      )}
       <h1 className="w-full text-xl leading-10 text-sbcBlue-500 text-center border-b-4 border-sbcBlue-500 mb-0.5">
         {title}
       </h1>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,45 @@
 import { Link } from 'react-router-dom'
 
+import './Home.css'
+
+import Footer from '../components/Footer'
+import Header from '../components/Header'
+
 export default function Home() {
   return (
-    <div className="flex flex-col h-screen w-screen">
-      <Link className="h-1/2 bg-sbcBlue-500" to="/games">
-        Uitslagen
-      </Link>
-      <Link className="h-1/2 bg-sbcOrange-500" to="/tables">
-        Standen
-      </Link>
+    <div className="w-screen h-screen flex justify-center">
+      <div className="flex flex-col w-full max-w-md h-screen">
+        <Header title="Home" hideBackButton={true}></Header>
+        <div className="h-full p-4 home-background text-sbcBlue-500 text-center text-lg">
+          <div className="h-full flex flex-col justify-evenly items-center home-player">
+            <BlockLink
+              to="/games"
+              title="Wedstrijden"
+              className="home-icon-games"
+            ></BlockLink>
+            <BlockLink
+              to="/tables"
+              title="Standen"
+              className="home-icon-tables"
+            ></BlockLink>
+          </div>
+        </div>
+        <Footer></Footer>
+      </div>
     </div>
+  )
+}
+
+function BlockLink(props: { to: string; title: string; className: string }) {
+  const { to, title, className } = props
+
+  return (
+    <Link to={to} className="w-2/3 bg-slate-50/80 rounded-lg aspect-square">
+      <div
+        className={`w-full h-full flex flex-col justify-end items-center p-4 ${className}`}
+      >
+        {title}
+      </div>
+    </Link>
   )
 }
